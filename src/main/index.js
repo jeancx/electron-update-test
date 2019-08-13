bbfeb897b9756429f5a5d3ac87ca78a3b684ea50
Jean Carlos Farias <contato@jeancf.com.br>
2019-08-13 16:07:57 -0300

    Working with electron packager

--- src/main/index.js
+++ src/main/index.js
@@ -7,7 +7,7 @@
   const Menu = electron.Menu
   const Tray = electron.Tray
   const BrowserWindow = electron.BrowserWindow
-  const autoUpdater = electron.autoUpdater
+  const { autoUpdater,  } = require("electron-updater")
   const dialog = electron.dialog
 
   let tray = null
@@ -94,30 +94,23 @@
   }
 
   const autoUpdate = () => {
-    const server = 'https://your-deployment-url.com'
-    const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+    autoUpdater.autoDownload = true
 
-    autoUpdater.setFeedURL(feed)
-
-    autoUpdater.on('error', message => {
-      console.error('Erro ao atualizar o aplicativo')
-      console.error(message)
-    })
-
-    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-      const dialogOpts = {
-        type: 'info',
-        buttons: ['Restart', 'Later'],
-        title: 'Application Update',
-        message: process.platform === 'win32' ? releaseNotes : releaseName,
-        detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-      }
-      dialog.showMessageBox(dialogOpts, (response) => {
-        if (response === 0) autoUpdater.quitAndInstall()
+    autoUpdater.on('update-avaliable', () => {
+      dialog.showMessageBox({
+        title: 'Atualizando',
+        message: 'Atualização disponível.'
       })
     })
 
-    autoUpdater.checkForUpdates()
+    autoUpdater.on('update-downloaded', () => {
+      autoUpdater.quitAndInstall(true, true)
+
+      setImmediate(() => autoUpdater.quitAndInstall())
+    })
+
+    setInterval(() => { autoUpdater.checkForUpdates() }, 10000)
+    autoUpdater.checkForUpdatesAndNotify()
   }
 
   app.on('before-quit', () => {
